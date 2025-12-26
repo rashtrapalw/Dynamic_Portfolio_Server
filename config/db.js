e6ec52eb022fd0424d0c5d6f40f3292262f9c830
rashtrapal143 <rashtraapalw143@gmail.com>
2025-12-26 23:16:48 +0530

    router error solve

--- config/db.js
+++ config/db.js
@@ -1,13 +1,28 @@
-const mongoose = require ('mongoose')
-const connection= (async()=>{
+// const mongoose = require ('mongoose')
+// const connection= (async()=>{
 
-    try{
-        await mongoose.connect (""+process.env.CONNECTION_STRING)
-        console.log("DB connected Succesfully"+mongoose.connection.readyState)
-    }catch(error){
-        console.log(error)
-         console.log("DB Connection Faild....." + mongoose.connection.readyState)
-    }
-})
-connection()
-    module.exports = connection
+//     try{
+//         await mongoose.connect (""+process.env.CONNECTION_STRING)
+//         console.log("DB connected Succesfully"+mongoose.connection.readyState)
+//     }catch(error){
+//         console.log(error)
+//          console.log("DB Connection Faild....." + mongoose.connection.readyState)
+//     }
+// })
+// connection()
+//     module.exports = connection
+
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log("✅ DB connected successfully");
+  } catch (error) {
+    console.error("❌ DB connection failed", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- model/portfolioSchema.js
+++ model/portfolioSchema.js
@@ -1,22 +1,60 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const portfolioSchema = new mongoose.Schema({
-  name: String,
-  title: String,
-  about: String,
-  skills: [String],
-  projects: [
-    {
-      title: String,
-      description: String,
-      github: String,
-      demo: String,
-    }
-  ],
-  contact: {
-    email: String,
-    phone: String
-  }
-});
+// const portfolioSchema = new mongoose.Schema({
+//   name: String,
+//   title: String,
+//   about: String,
+//   skills: [String],
+//   projects: [
+//     {
+//       title: String,
+//       description: String,
+//       github: String,
+//       demo: String,
+//     }
+//   ],
+//   contact: {
+//     email: String,
+//     phone: String
+//   }
+// });
 
-module.exports = mongoose.model('Portfolio', portfolioSchema);
+// module.exports = mongoose.model('Portfolio', portfolioSchema);
+
+
+
+const mongoose = require("mongoose");
+
+const portfolioSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+    },
+    skills: {
+      type: [String],
+    },
+    projects: [
+      {
+        title: String,
+        description: String,
+        github: String,
+        demo: String,
+      },
+    ],
+    contact: {
+      email: String,
+      phone: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Portfolio", portfolioSchema);
